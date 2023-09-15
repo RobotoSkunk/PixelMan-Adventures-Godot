01f8995ef78cff6db5eea3ab50ed8056472ce64e
RobotoSkunk <contact@robotoskunk.com>
2023-09-15 16:50:57 -0600

    Added script to player and moved everything to the correct path

---
+++ objects/player/Player.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+
+namespace RobotoSkunk.PixelMan.Objects
+{
+	public class Player : Node2D
+	{
+		public override void _Ready()
+		{
+			GD.Print("Player ready");
+		}
+
+		// public override void _Process(double delta)
+		// {
+		// }
+	}
+}
